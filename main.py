6fa2fcf2b42c3240127a26bd8b5f51cfdec2e072
Vinicius Santos <vinobeacoup56@gmail.com>
2026-07-14 16:04:46 -0300

    Add headless option for browser automation

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ indexmunicipio = int(os.getenv("INDEXMUNICIPIO"))
 indexbairro = int(os.getenv("INDEXBAIRRO"))
 
 options = Options()
-
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--window-size=1920,1080")
